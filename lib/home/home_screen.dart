d0ceaae322de96a1a4b30dacf720442fa07bbfb1
Eleaus Hossain Evan <eleaus16-364@diu.edu.bd>
2026-05-12 00:34:11 +0600

    feat: UI switch bound to onlineProvider

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import '../providers/online_provider.dart';
+import '../providers/socket_connection_provider.dart';
 
 class HomeScreen extends HookConsumerWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final online = useState<bool>(false);
+    final isOnline = ref.watch(onlineProvider);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
       body: Column(
@@ -15,10 +18,12 @@ class HomeScreen extends HookConsumerWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Switch(
-            value: online.value,
-            onChanged: (value) => online.value = value,
+            value: isOnline,
+            onChanged: (value) async {
+              await ref.read(onlineProvider.notifier).set(value);
+            },
           ),
-          Text(
+          const Text(
             'Go Online',
             textAlign: TextAlign.center,
           ),
