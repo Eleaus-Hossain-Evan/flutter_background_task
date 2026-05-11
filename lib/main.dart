04600ca0424ee6b0ced564ebd28b82cb46908df3
Eleaus Hossain Evan <eleaus16-364@diu.edu.bd>
2026-05-12 00:33:53 +0600

    chore: initialise background service and hydrate online flag

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_background_task/home/home_screen.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'background/background_entry.dart';
+import 'providers/online_provider.dart';
 
-void main() {
+final _rootContainer = ProviderContainer();
+final ref = _rootContainer.ref;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await ref.read(onlineProvider.notifier).init();
+
+  await FlutterBackgroundService.initialize(
+    androidConfiguration: AndroidConfiguration(
+      notificationTitle: "Background Socket",
+      notificationContent: "Running...",
+      foregroundMode: true,
+    ),
+    iosConfiguration: null,
+  );
+
+  FlutterBackgroundService().setBackgroundService(
+    onStart: backgroundEntryPoint,
+  );
+
   runApp(
     ProviderScope(
       child: const MyApp(),
@@ -17,8 +40,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo BG Task',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: HomeScreen(),
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      home: const HomeScreen(),
     );
   }
 }
