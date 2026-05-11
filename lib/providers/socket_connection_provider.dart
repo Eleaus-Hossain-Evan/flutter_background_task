9b180e12c49ab789e9887c86fb3b60acc9118ec7
Eleaus Hossain Evan <eleaus16-364@diu.edu.bd>
2026-05-12 00:32:07 +0600

    feat: socket connection provider that reacts to online state

---
+++ lib/providers/socket_connection_provider.dart
@@ -0,0 +1,24 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import '../services/socket_service_provider.dart';
+import 'online_provider.dart';
+
+part 'socket_connection_provider.g.dart';
+
+@riverpod
+class SocketConnection extends _$SocketConnection {
+  @override
+  FutureOr<bool> build() async {
+    final isOnline = ref.watch(onlineProvider);
+    final socket = ref.watch(socketServiceProvider);
+
+    if (isOnline) {
+      await socket.connect();
+      return true;
+    } else {
+      await socket.disconnect();
+      return false;
+    }
+  }
+
+  Future<void> refresh() async => await ref.refresh(socketConnectionProvider.future);
+}
---
+++ lib/providers/socket_connection_provider.g.dart
@@ -0,0 +1,54 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'socket_connection_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(SocketConnection)
+final socketConnectionProvider = SocketConnectionProvider._();
+
+final class SocketConnectionProvider
+    extends $AsyncNotifierProvider<SocketConnection, bool> {
+  SocketConnectionProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'socketConnectionProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$socketConnectionHash();
+
+  @$internal
+  @override
+  SocketConnection create() => SocketConnection();
+}
+
+String _$socketConnectionHash() => r'33261061c7ca1dec270ea4144849265ff5dcc4a0';
+
+abstract class _$SocketConnection extends $AsyncNotifier<bool> {
+  FutureOr<bool> build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final ref = this.ref as $Ref<AsyncValue<bool>, bool>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<AsyncValue<bool>, bool>,
+              AsyncValue<bool>,
+              Object?,
+              Object?
+            >;
+    element.handleCreate(ref, build);
+  }
+}
